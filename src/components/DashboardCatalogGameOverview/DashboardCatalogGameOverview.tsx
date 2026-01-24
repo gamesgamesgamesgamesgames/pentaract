7f938f7234d73ed455d0035f7271730bd07a069f
Trezy <tre@trezy.com>
2026-01-24 14:09:29 -0600

    feat: add all game details to overview

--- src/components/DashboardCatalogGameOverview/DashboardCatalogGameOverview.tsx
+++ src/components/DashboardCatalogGameOverview/DashboardCatalogGameOverview.tsx
@@ -2,53 +2,20 @@
 
 // Module imports
 import { DataList, Flex, Text } from '@radix-ui/themes'
-import { ReactNode, useMemo } from 'react'
 
 // Local imports
-import { GAME_MODES } from '@/constants/GAME_MODES'
+import { CommaSeparatedList } from '../CommaSeparatedList/CommaSeparatedList'
 import { GAME_APPLICATION_TYPES } from '@/constants/GAME_APPLICATION_TYPES'
-import { Link } from '@/components/Link/Link'
+import { GAME_MODES } from '@/constants/GAME_MODES'
+import { GAME_GENRES } from '@/constants/GAME_GENRES'
+import { GAME_PLAYER_PERSPECTIVES } from '@/constants/GAME_PLAYER_PERSPECTIVES'
+import { GAME_THEMES } from '@/constants/GAME_THEMES'
 import { useDashboardCatalogGameContext } from '@/context/DashboardCatalogGameContext/DashboardCatalogGameContext'
 
 export function DashboardCatalogGameOverview() {
 	const { game } = useDashboardCatalogGameContext()
 
 	const gameRecord = game?.record!
-
-	const typeValue = useMemo(() => {
-		if (gameRecord.applicationType) {
-			return (
-				<Link
-					href={`/dashboard/catalog?applicationType=${gameRecord.applicationType}`}>
-					<Text>{GAME_APPLICATION_TYPES[gameRecord.applicationType].name}</Text>
-				</Link>
-			)
-		}
-
-		return <Text>{'No type provided'}</Text>
-	}, [gameRecord])
-
-	const modesValue = useMemo(() => {
-		if (gameRecord.modes) {
-			return gameRecord.modes.reduce((accumulator, mode, index, array) => {
-				accumulator.push(
-					<Link
-						key={mode}
-						href={`/dashboard/catalog?mode=${mode}`}>
-						<Text>{GAME_MODES[mode].name}</Text>
-					</Link>,
-				)
-
-				if (index < array.length - 1) {
-					accumulator.push(', ')
-				}
-
-				return accumulator
-			}, [] as ReactNode[])
-		}
-
-		return <Text>{'No modes provided'}</Text>
-	}, [gameRecord])
 
 	return (
 		<Flex direction={'column'}>
@@ -58,14 +25,102 @@ export function DashboardCatalogGameOverview() {
 
 			<DataList.Root>
 				<DataList.Item>
-					<DataList.Label>{'Type'}</DataList.Label>
-					<DataList.Value>{typeValue}</DataList.Value>
+					<DataList.Label>{'Name'}</DataList.Label>
+					<DataList.Value>{gameRecord.name}</DataList.Value>
 				</DataList.Item>
-
+				<DataList.Item>
+					<DataList.Label>{'Summary'}</DataList.Label>
+					<DataList.Value>
+						{gameRecord.summary || (
+							<Text
+								color={'gold'}
+								style={{ fontStyle: 'italic' }}>
+								{'No summary provided.'}
+							</Text>
+						)}
+					</DataList.Value>
+				</DataList.Item>
+				<DataList.Item>
+					<DataList.Label>{'Type'}</DataList.Label>
+					<DataList.Value>
+						{gameRecord.applicationType
+							? GAME_APPLICATION_TYPES[gameRecord.applicationType].name
+							: ''}
+					</DataList.Value>
+				</DataList.Item>
+				<DataList.Item>
+					<DataList.Label>{'Genres'}</DataList.Label>
+					<DataList.Value>
+						{gameRecord.genres?.length ? (
+							<CommaSeparatedList
+								includeLinks
+								items={gameRecord.genres.map(
+									(genre) => GAME_GENRES[genre]!.name,
+								)}
+							/>
+						) : (
+							<Text
+								color={'gold'}
+								style={{ fontStyle: 'italic' }}>
+								{'No genres provided'}
+							</Text>
+						)}
+					</DataList.Value>
+				</DataList.Item>
+				<DataList.Item>
+					<DataList.Label>{'Themes'}</DataList.Label>
+					<DataList.Value>
+						{gameRecord.themes?.length ? (
+							<CommaSeparatedList
+								includeLinks
+								items={gameRecord.themes.map(
+									(theme) => GAME_THEMES[theme]!.name,
+								)}
+							/>
+						) : (
+							<Text
+								color={'gold'}
+								style={{ fontStyle: 'italic' }}>
+								{'No themes provided'}
+							</Text>
+						)}
+					</DataList.Value>
+				</DataList.Item>
+				<DataList.Item>
+					<DataList.Label>{'Player Perspectives'}</DataList.Label>
+					<DataList.Value>
+						{gameRecord.playerPerspectives?.length ? (
+							<CommaSeparatedList
+								includeLinks
+								items={gameRecord.playerPerspectives.map(
+									(playerPerspective) =>
+										GAME_PLAYER_PERSPECTIVES[playerPerspective]!.name,
+								)}
+							/>
+						) : (
+							<Text
+								color={'gold'}
+								style={{ fontStyle: 'italic' }}>
+								{'No player perspectives provided'}
+							</Text>
+						)}
+					</DataList.Value>
+				</DataList.Item>
 				<DataList.Item>
 					<DataList.Label>{'Modes'}</DataList.Label>
 					<DataList.Value>
-						<Text>{modesValue}</Text>
+						{gameRecord.modes?.length ? (
+							<CommaSeparatedList
+								includeLinks
+								items={gameRecord.modes.map((mode) => GAME_MODES[mode]!.name)}
+							/>
+						) : (
+							<Text
+								color={'gold'}
+								style={{ fontStyle: 'italic' }}>
+								{'No modes provided'}
+							</Text>
+						)}
 					</DataList.Value>
 				</DataList.Item>
 			</DataList.Root>
