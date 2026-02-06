f539e4b7a1c6a67c535dee98f6a4ac377cfc320b
Trezy <tre@trezy.com>
2026-02-06 16:28:07 -0600

    feat: render all media on game pages

--- src/components/GamePage/Media.tsx
+++ src/components/GamePage/Media.tsx
@@ -1,36 +1,57 @@
+// Module imports
+import { Fragment } from 'react'
+
 // Local imports
-import { AspectRatio } from '@/components/ui/aspect-ratio'
 import { Card } from '@/components/ui/card'
 import { type GameRecord } from '@/typedefs/GameRecord'
 import { Header } from '@/components/Header/Header'
+import { MediaType } from '@/typedefs/MediaType'
 
 // Types
+type GameMediaItem = NonNullable<GameRecord['media']>[number]
 type Props = Readonly<{ gameRecord: GameRecord }>
 
 export function Media(props: Props) {
 	const { gameRecord } = props
 
-	const screenshots = gameRecord?.media?.filter(
-		(item) => item.mediaType === 'screenshot',
+	if (!gameRecord.media?.length) {
+		return null
+	}
+
+	const mediaItemFilters = gameRecord?.media?.reduce(
+		(accumulator, mediaItem) => {
+			const mediaType = mediaItem.mediaType!
+			if (!accumulator[mediaType]) {
+				accumulator[mediaType] = []
+			}
+
+			accumulator[mediaType].push(mediaItem)
+
+			return accumulator
+		},
+		{} as Record<MediaType, GameMediaItem[]>,
 	)
 
 	return (
 		<section>
 			<Card className={'flex p-4'}>
-				<Header level={3}>{'Screenshots'}</Header>
+				<Header level={3}>{'Media'}</Header>
 
-				<div className={'col-span-2 gap-4 grid grid-cols-3'}>
-					{Boolean(screenshots) &&
-						screenshots!.map((screenshot) => (
-							<AspectRatio ratio={16 / 9}>
+				{Object.entries(mediaItemFilters).map(([mediaType, mediaItems]) => (
+					<Fragment key={mediaType}>
+						<Header level={4}>{mediaType}</Header>
+						<div className={'col-span-2 gap-4 grid grid-cols-3'}>
+							{mediaItems.map((mediaItem) => (
 								<img
+									key={mediaItem.blob!.url}
 									alt={`Screenshot from ${gameRecord?.name}`}
-									className={'relative h-full w-full object-cover'}
-									src={screenshot.blob?.url ?? 'https://placehold.co/320x180'}
+									className={'relative h-full w-full object-contain'}
+									src={mediaItem.blob!.url ?? 'https://placehold.co/320x180'}
 								/>
-							</AspectRatio>
-						))}
-				</div>
+							))}
+						</div>
+					</Fragment>
+				))}
 			</Card>
 		</section>
 	)
